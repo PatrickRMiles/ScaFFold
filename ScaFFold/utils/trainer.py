5068938dacf9f66e70a2a83982fc040e36daa6ca
Patrick Miles <miles30@tioga.llnl.gov>
2026-04-02 16:09:34 -0700

    move dataloader changes to new branch to resolve git weirdness

--- ScaFFold/utils/trainer.py
+++ ScaFFold/utils/trainer.py
@@ -133,11 +133,17 @@ class BaseTrainer:
         self.create_dataset()
         self.create_sampler()
 
+        num_workers = self.config.dataloader_num_workers
         loader_args = dict(
-            batch_size=self.config.batch_size, num_workers=1, pin_memory=True
+            batch_size=self.config.batch_size,
+            num_workers=num_workers,
+            pin_memory=True,
         )
+        if num_workers > 0:
+            loader_args["persistent_workers"] = True
+            loader_args["prefetch_factor"] = 2
         self.log.debug(
-            f"dataloader num_workers={loader_args['num_workers']}, os.cpu_count()={os.cpu_count()}, self.world_size={self.world_size} "
+            f"dataloader num_workers={loader_args['num_workers']}, prefetch_factor={loader_args.get('prefetch_factor')}, persistent_workers={loader_args.get('persistent_workers', False)}, os.cpu_count()={os.cpu_count()}, self.world_size={self.world_size} "
         )
         self.train_loader = DataLoader(
             self.train_set, sampler=self.train_sampler, **loader_args
@@ -207,6 +213,10 @@ class PyTorchTrainer(BaseTrainer):
             # Check config for async setting, default to False
             async_save=getattr(self.config, "async_save", False),
         )
+
+        self.ps = None  # DistConv ParallelStrategy
+        self.spatial_mesh = None  # Spatial mesh for use w/ DistConv
+        self.ddp_placements = None  # DDP placements for use w/ DistConv
 
     def cleanup_or_resume(self):
         """
